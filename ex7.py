8bc3bb0f7af576904ebb57e841673ae56d8f5e23
Felipe Savoia <fmsavoia@gmail.com>
2016-08-31 00:06:46 -0300

    New exercices

---
+++ ex7.py
@@ -0,0 +1,30 @@
+# Only print the string below
+print "Mary had a little lamb."
+
+# In this case, the %s was replaced by string "snow". If there wasn't the "" character in the word snow, the %s would be replaced by variable 'snow', which doesn't exist.
+print "Its fleece was white as %s." % "snow"
+
+# Only print the string below
+print "And everywhere that Mary went."
+
+# The string . repeated 10 times
+print "." * 10 # what'd that do?
+
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "s"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# watch that comma at the end. try removing it to see what happens
+
+# The comman concatenate the output
+print end1 + end2 + end3 + end4 + end5 + end6,
+print end7 + end8 + end9 + end10 + end11 + end12
